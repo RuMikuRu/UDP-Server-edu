f6b5807760c27223e84a03b10faf83db2816015f
iliya <MikuHutsune.Ru@yandex.ru>
2022-11-28 12:00:03 +0300

    v1.0

---
+++ src/Server.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Server extends Thread {
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buf = new byte[256];
+
+    public Server() throws SocketException {
+        socket = new DatagramSocket(5001);
+
+    }
+
+    public void run()
+    {
+        running = true;
+
+        while (running)
+        {
+            System.out.println("Start");
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(buf, buf.length, address, port);
+            String receive = new String(packet.getData(), 0, packet.getLength());
+
+            if(receive.equals("end"))
+            {
+                running = false;
+                continue;
+            }
+            else
+            {
+                System.out.println(receive);
+            }
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        socket.close();
+    }
+}
